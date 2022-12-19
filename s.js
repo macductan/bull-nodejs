d482631ece81cfce47189974ffbddb0e0d3b30cc
Mạc Đức Tân <73536823+macductan@users.noreply.github.com>
2022-12-19 15:30:00 +0700

    update

--- s.js
+++ s.js
@@ -1,29 +1,70 @@
-// subscriber.js
+const Queue = require('bull');
 
-const Redis = require("ioredis");
-const redis = new Redis();
+const videoQueue = new Queue('video transcoding', 'redis://127.0.0.1:6379');
+const audioQueue = new Queue('audio transcoding', { redis: { port: 6379, host: '127.0.0.1', password: 'foobared' } }); // Specify Redis connection using object
+const imageQueue = new Queue('image transcoding');
+const pdfQueue = new Queue('pdf transcoding');
 
-redis.subscribe("my-channel-1", "my-channel-2", (err, count) => {
-  if (err) {
-    // Just like other commands, subscribe() can fail for some reasons,
-    // ex network issues.
-    console.error("Failed to subscribe: %s", err.message);
-  } else {
-    // `count` represents the number of channels this client are currently subscribed to.
-    console.log(
-      `Subscribed successfully! This client is currently subscribed to ${count} channels.`
-    );
-  }
-});
+// videoQueue.process(function (job, done) {
 
-redis.on("message", (channel, message) => {
-  console.log(`Received ${message} from ${channel}`);
-});
+//   // job.data contains the custom data passed when the job was created
+//   // job.id contains id of this job.
 
-// There's also an event called 'messageBuffer', which is the same as 'message' except
-// it returns buffers instead of strings.
-// It's useful when the messages are binary data.
-redis.on("messageBuffer", (channel, message) => {
-  // Both `channel` and `message` are buffers.
-  console.log(channel, message);
-});
+//   // transcode video asynchronously and report progress
+//   job.progress(42);
+
+//   // call done when finished
+//   done();
+
+//   // or give an error if error
+//   done(new Error('error transcoding'));
+
+//   // or pass it a result
+//   done(null, { framerate: 29.5 /* etc... */ });
+
+//   // If the job throws an unhandled exception it is also handled correctly
+//   throw new Error('some unexpected error');
+// });
+
+// audioQueue.process(function (job, done) {
+//   // transcode audio asynchronously and report progress
+//   job.progress(42);
+
+//   // call done when finished
+//   done();
+
+//   // or give an error if error
+//   done(new Error('error transcoding'));
+
+//   // or pass it a result
+//   done(null, { samplerate: 48000 /* etc... */ });
+
+//   // If the job throws an unhandled exception it is also handled correctly
+//   throw new Error('some unexpected error');
+// });
+
+// imageQueue.process(function (job, done) {
+//   // transcode image asynchronously and report progress
+//   job.progress(42);
+
+//   // call done when finished
+//   done();
+
+//   // or give an error if error
+//   done(new Error('error transcoding'));
+
+//   // or pass it a result
+//   done(null, { width: 1280, height: 720 /* etc... */ });
+
+//   // If the job throws an unhandled exception it is also handled correctly
+//   throw new Error('some unexpected error');
+// });
+
+// pdfQueue.process(function (job) {
+//   // Processors can also return promises instead of using the done callback
+//   return pdfAsyncProcessor();
+// });
+
+videoQueue.add({ video: 'http://example.com/video1.mov' });
+audioQueue.add({ audio: 'http://example.com/audio1.mp3' });
+imageQueue.add({ image: 'http://example.com/image1.tiff' });
